a74fd9d187ce988e8296b2fdc15c13f719effde4
Matt Hanlon <mhanlon@thecodehub.ie>
2021-03-02 08:30:28 +0000

    Initial Commit

---
+++ GettingStarted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Matthew Hanlon on 02/03/2021.
+//  Copyright © 2021 Teaching Develop in Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
